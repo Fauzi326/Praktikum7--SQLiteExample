524ac66a7bb25cd16a331e88d3c82181de425471
ACER <m.fauzi30052004@gmail.com>
2024-11-18 03:09:01 +0700

    add StudentModel.kt

--- app/src/main/java/muhammadfauzi/polbeng/ac/id/sqliteexample_p7/StudentModel.kt
+++ app/src/main/java/muhammadfauzi/polbeng/ac/id/sqliteexample_p7/StudentModel.kt
@@ -1,7 +1,11 @@
 package muhammadfauzi.polbeng.ac.id.sqliteexample_p7
 
-class StudentModel (
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class StudentModel (
     val nim: String,
     val name: String,
     val age: String
-)
+): Parcelable
