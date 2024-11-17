34ba5fe0ee0f1f0e630f5b16b03018176a9d426f
ACER <m.fauzi30052004@gmail.com>
2024-11-18 03:08:06 +0700

    add MainActivity.kt

--- app/src/main/java/muhammadfauzi/polbeng/ac/id/sqliteexample_p7/MainActivity.kt
+++ app/src/main/java/muhammadfauzi/polbeng/ac/id/sqliteexample_p7/MainActivity.kt
@@ -1,17 +1,22 @@
 package muhammadfauzi.polbeng.ac.id.sqliteexample_p7
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import muhammadfauzi.polbeng.ac.id.sqliteexample_p7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var studentDBHelper: StudentDBHelper
+    private var listData: ArrayList<StudentModel> = arrayListOf()
+    private lateinit var studentAdapter: StudentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        studentAdapter = StudentAdapter(listData)
+        initilizeRecyclerList()
         setContentView(binding.root)
         studentDBHelper = StudentDBHelper(this)
         binding.btnCari.setOnClickListener {
@@ -21,22 +26,14 @@ class MainActivity : AppCompatActivity() {
                     return@setOnClickListener
             }
             val students = studentDBHelper.searchStudentByName(nama)
-            binding.llData.removeAllViews()
-            students.forEach {
-                val tvUser = TextView(this)
-                tvUser.textSize = 20F
-                tvUser.text = "${it.nim} - ${it.name}(${it.age}Tahun)"
-                binding.llData.addView(tvUser)
-            }
+            listData.clear()
+            listData.addAll(students)
+            studentAdapter.notifyDataSetChanged()
             binding.tvHasilPencarian.text = "Ditemukan${students.size} mahasiswa"
         }
         binding.btnTambah.setOnClickListener {
             startActivity(Intent(this@MainActivity,
                 CreateActivity::class.java))
-        }
-        binding.btnUpdate.setOnClickListener {
-            startActivity(Intent(this@MainActivity,
-                UpdateActivity::class.java))
         }
         binding.swipeRefresh.setOnRefreshListener {
             binding.swipeRefresh.isRefreshing = true
@@ -45,14 +42,10 @@ class MainActivity : AppCompatActivity() {
     }
     private fun loadAllData(){
         val students = studentDBHelper.readStudents()
-        binding.llData.removeAllViews()
-        students.forEach {
-            val tvUser = TextView(this)
-            tvUser.textSize = 20F
-            tvUser.text = "${it.nim} - ${it.name}(${it.age} Tahun)"
-            binding.llData.addView(tvUser)
-        }
-        binding.tvHasilPencarian.text = "Total ${students.size}mahasiswa"
+        listData.clear()
+        listData.addAll(students)
+        studentAdapter.notifyDataSetChanged()
+        binding.tvHasilPencarian.text = "Total ${students.size} mahasiswa"
         binding.swipeRefresh.isRefreshing = false
     }
     override fun onResume() {
@@ -62,5 +55,25 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         studentDBHelper.close()
         super.onDestroy()
+    }
+    private fun initilizeRecyclerList(){
+        binding.rvStudents.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = studentAdapter
+        }
+        studentAdapter.setOnItemClickCallback(object :
+            StudentAdapter.OnItemClickCallback{
+            override fun onItemClicked(data: StudentModel) {
+                updateStudent(data)
+            }
+        })
+    }
+    private fun updateStudent(data: StudentModel){
+        val moveWithObjectIntent = Intent(this@MainActivity,
+            UpdateActivity::class.java)
+        moveWithObjectIntent.putExtra(UpdateActivity.EXTRA_STUDENT,
+            data)
+        startActivity(moveWithObjectIntent)
     }
 }
